c81b87b74ba0c2c0a10b2520f3460e83acc093f6
Michele Righi <righi.michy@gmail.com>
2021-12-31 00:14:39 +0100

    update

--- app-mobile/flutter_application/lib/views/ViewSettings.dart
+++ app-mobile/flutter_application/lib/views/ViewSettings.dart
@@ -1,34 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:roquiz/constants.dart';
-import 'package:roquiz/widget/change_theme_button.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 class ViewSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final text = MediaQuery.of(context).platformBrightness == Brightness.dark
-        ? 'DarkTheme'
-        : 'LightTheme';
-
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: const Text("Impostazioni"),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        actions: [
-          ChangeThemeButtonWidget(),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'Hello $text!',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
