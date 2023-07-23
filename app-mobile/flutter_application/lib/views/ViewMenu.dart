7ea8e2f212d92f50cae4026aaf0e811a5c5ad75b
Michele Righi <righi.michy@gmail.com>
2023-07-24 00:53:27 +0200

    risolto bug #10

--- app-mobile/flutter_application/lib/views/ViewMenu.dart
+++ app-mobile/flutter_application/lib/views/ViewMenu.dart
@@ -30,8 +30,8 @@ class ViewMenuState extends State<ViewMenu> {
   final List<bool> _selectedTopics = [];
   int _quizPool = 0;
 
-  late Future<int> _quizQuestionNumber;
-  late Future<int> _timer;
+  late int _quizQuestionNumber;
+  late int _timer;
 
   void _initTopics() {
     setState(() {
@@ -97,8 +97,6 @@ class ViewMenuState extends State<ViewMenu> {
           Settings.VERSION_NUMBER = packageInfo.version;
         },
       );
-
-      //print(Settings.VERSION_NUMBER);
       //print(packageInfo.buildNumber);
     });
 
@@ -118,12 +116,10 @@ class ViewMenuState extends State<ViewMenu> {
         .onError((error, stackTrace) =>
             {setState(() => _qRepoLoadingError = error.toString())});
 
-    _quizQuestionNumber = _prefs.then((SharedPreferences prefs) {
-      return prefs.getInt("questionNumber") ?? -1;
-    });
-    _timer = _prefs.then((SharedPreferences prefs) {
-      return prefs.getInt("timer") ?? -1;
-    });
+    _prefs.then((value) => (SharedPreferences prefs) {
+          _quizQuestionNumber = prefs.getInt("questionNumber") ?? -1;
+          _timer = prefs.getInt("timer") ?? -1;
+        });
   }
 
   Future<void> _launchInBrowser(String url) async {
@@ -136,154 +132,144 @@ class ViewMenuState extends State<ViewMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: FutureBuilder<List>(
-              future: Future.wait([_quizQuestionNumber, _timer]),
-              builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return const CircularProgressIndicator();
-                  default:
-                    if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else {
-                      return Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              const Spacer(flex: 2),
-                              const Text("ROQuiz",
-                                  style: TextStyle(
-                                    fontSize: 54,
-                                    fontWeight: FontWeight
-                                        .bold, /*fontStyle: FontStyle.italic*/
-                                  )),
-                              Text(
-                                "v${Settings.VERSION_NUMBER}",
-                                style: const TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Spacer(flex: 1),
-                              // BUTTONS
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 30.0),
-                                child: ElevatedButton(
-                                  onPressed: qRepo.questions.isEmpty
-                                      ? null
-                                      : () {
-                                          Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      ViewQuiz(
-                                                        questions:
-                                                            _getPoolFromSelected(),
-                                                        settings: _settings,
-                                                      )));
-                                        },
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 60,
-                                    child: const Text(
-                                      "Avvia",
-                                      style: TextStyle(
-                                        fontSize: 26,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 30.0),
-                                child: ElevatedButton(
-                                  onPressed: _topicsPresent
-                                      ? () {
-                                          Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      ViewTopics(
-                                                        qRepo: qRepo,
-                                                        settings: _settings,
-                                                        updateQuizPool:
-                                                            updateQuizPool,
-                                                        selectedTopics:
-                                                            _selectedTopics,
-                                                      )));
-                                        }
-                                      : null,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 60,
-                                    child: const Text(
-                                      "Argomenti",
-                                      style: TextStyle(
-                                        fontSize: 26,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const Icon(
-                                      Icons.format_list_numbered_rounded,
-                                    ),
-                                    Text(
-                                        "Domande: ${_settings.questionNumber} su $_quizPool"),
-                                    const SizedBox(width: 20),
-                                    const Icon(
-                                      Icons.timer_rounded,
-                                    ),
-                                    Text("Tempo: ${_settings.timer} min"),
-                                  ]),
-                              _qRepoLoadingError.isNotEmpty
-                                  ? Padding(
-                                      padding: const EdgeInsets.all(15.0),
-                                      child: Text(
-                                        _qRepoLoadingError,
-                                        style:
-                                            const TextStyle(color: Colors.red),
-                                      ))
-                                  : const SizedBox(height: 50),
-                              InkWell(
-                                onTap: () {
-                                  _launchInBrowser(
-                                      "https://github.com/mikyll/ROQuiz");
+      body: Center(child: FutureBuilder<List>(
+          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const CircularProgressIndicator();
+          default:
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Spacer(flex: 2),
+                      const Text("ROQuiz",
+                          style: TextStyle(
+                            fontSize: 54,
+                            fontWeight:
+                                FontWeight.bold, /*fontStyle: FontStyle.italic*/
+                          )),
+                      Text(
+                        "v${Settings.VERSION_NUMBER}",
+                        style: const TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(flex: 1),
+                      // BUTTONS
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: ElevatedButton(
+                          onPressed: qRepo.questions.isEmpty
+                              ? null
+                              : () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ViewQuiz(
+                                                questions:
+                                                    _getPoolFromSelected(),
+                                                settings: _settings,
+                                              )));
                                 },
-                                child: Container(
-                                  color: Colors.indigo.withOpacity(0.35),
-                                  height: 120,
-                                  alignment: Alignment.center,
-                                  child: const Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text(
-                                        "Se l'app ti è piaciuta, considera di lasciare una stellina alla repository GitHub!\n\nBasta un click qui!",
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                        )),
-                                  ),
-                                ),
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 60,
+                            child: const Text(
+                              "Avvia",
+                              style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
                               ),
-                              const Spacer(flex: 5),
-                            ],
-                          ));
-                    }
-                }
-              })),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: ElevatedButton(
+                          onPressed: _topicsPresent
+                              ? () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ViewTopics(
+                                                qRepo: qRepo,
+                                                settings: _settings,
+                                                updateQuizPool: updateQuizPool,
+                                                selectedTopics: _selectedTopics,
+                                              )));
+                                }
+                              : null,
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 60,
+                            child: const Text(
+                              "Argomenti",
+                              style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.format_list_numbered_rounded,
+                            ),
+                            Text(
+                                "Domande: ${_settings.questionNumber} su $_quizPool"),
+                            const SizedBox(width: 20),
+                            const Icon(
+                              Icons.timer_rounded,
+                            ),
+                            Text("Tempo: ${_settings.timer} min"),
+                          ]),
+                      _qRepoLoadingError.isNotEmpty
+                          ? Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text(
+                                _qRepoLoadingError,
+                                style: const TextStyle(color: Colors.red),
+                              ))
+                          : const SizedBox(height: 50),
+                      InkWell(
+                        onTap: () {
+                          _launchInBrowser("https://github.com/mikyll/ROQuiz");
+                        },
+                        child: Container(
+                          color: Colors.indigo.withOpacity(0.35),
+                          height: 120,
+                          alignment: Alignment.center,
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                                "Se l'app ti è piaciuta, considera di lasciare una stellina alla repository GitHub!\n\nBasta un click qui!",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                )),
+                          ),
+                        ),
+                      ),
+                      const Spacer(flex: 5),
+                    ],
+                  ));
+            }
+        }
+      })),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
