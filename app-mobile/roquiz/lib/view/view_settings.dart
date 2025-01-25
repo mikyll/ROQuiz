a7af7551cf1d3980305678325c7b43f9dae68672
mikyll <righi.michy@gmail.com>
2025-01-25 03:09:58 +0100

    refactor: view settings

---
+++ app-mobile/roquiz/lib/view/view_settings.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:roquiz/model/persistence/setting_type.dart';
+import 'package:roquiz/model/persistence/settings.dart';
+import 'package:roquiz/model/themes.dart';
+import 'package:roquiz/widget/back_button.dart';
+import 'package:roquiz/widget/settings/setting_checkbox.dart';
+import 'package:roquiz/widget/settings/setting_checkbox_button.dart';
+import 'package:roquiz/widget/settings/setting_spinner.dart';
+import 'package:roquiz/widget/settings/setting_switch.dart';
+
+class ViewSettings extends StatefulWidget {
+  const ViewSettings({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ViewSettingsState();
+}
+
+class ViewSettingsState extends State<ViewSettings> {
+  final ScrollController _scrollController = ScrollController();
+  Settings _settings = Settings.fromDefault();
+
+  void _goBack(context, {bool showDialog = false}) {
+    bool shouldPop = true;
+
+    if (showDialog) {
+      // TODO
+      // shouldPop = await dialog ?? false;
+    }
+
+    if (shouldPop) {
+      Navigator.pop(context);
+    }
+  }
+
+  void _initSettings() {
+    Settings.loadFromSharedPreferences().then((settingsPref) {
+      setState(() {
+        _settings = settingsPref;
+      });
+    });
+  }
+
+  List<Widget> _buildSettingEntriesList() {
+    List<Widget> widgets = [];
+
+    // TEST
+    Settings s = Settings.fromDefault();
+
+    // TODO
+    for (String key in s.getKeys()) {
+      Widget w;
+
+      SettingType type = s.getType(key);
+      String label = s.getLabel(key);
+      String description = s.getDescription(key);
+
+      switch (type) {
+        case SettingType.toggleCheckbox:
+          w = SettingCheckboxWidget(
+            label: label,
+            description: description,
+            onChanged: (_) {},
+          );
+          break;
+
+        case SettingType.toggleSwitch:
+          w = SettingSwitchWidget(
+            label: label,
+            description: description,
+            onChanged: (_) {},
+          );
+          break;
+
+        case SettingType.spinner:
+          w = SettingSpinnerWidget(
+            label: label,
+            description: description,
+            onChanged: (_) {},
+          );
+          break;
+
+        case SettingType.toggleCheckboxAndButton:
+          w = SettingCheckboxAndButtonWidget(
+            label: label,
+            description: description,
+            onChanged: (_) {},
+            onClick: () {},
+          );
+          break;
+        default:
+          w = Text("Error");
+      }
+      widgets.add(w);
+    }
+
+    // widgets.addAll(
+    //   [
+    //     Switch(
+    //       value: themeProvider.themeMode == ThemeMode.dark,
+    //       onChanged: (_) {
+    //         themeProvider.setTheme(ThemeMode.system);
+    //       },
+    //     ),
+    //     Switch(
+    //       value: themeProvider.themeMode == ThemeMode.dark,
+    //       onChanged: (isDarkMode) {
+    //         themeProvider.toggleTheme();
+    //       },
+    //     ),
+    //   ],
+    // );
+
+    return widgets;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context, listen: true);
+    bool canPop = false;
+
+    return PopScope(
+      canPop: canPop,
+      onPopInvokedWithResult: (bool didPop, Object? result) {
+        if (didPop) {
+          return;
+        }
+        _goBack(context);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Impostazioni"),
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          leading: BackButtonWidget(),
+        ),
+        body: Center(
+          child: ListView(
+              padding: const EdgeInsets.all(15.0),
+              controller: _scrollController,
+              shrinkWrap: true,
+              primary: false,
+              children: _buildSettingEntriesList()),
+        ),
+      ),
+    );
+  }
+}
