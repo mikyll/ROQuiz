fac4f96e6b01771d2096d06be059b492db6ebaa2
Michele Righi <righi.michy@gmail.com>
2025-06-30 23:48:31 +0200

    style: change all CRLF to LF

--- app-mobile/flutter_application/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app-mobile/flutter_application/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import desktop_window
+import file_picker
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -13,6 +14,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
