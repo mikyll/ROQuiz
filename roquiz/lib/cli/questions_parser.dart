fdf4e698379d81ab7e042e9009e4b2438377f6de
Michele Righi <righi.michy@gmail.com>
2025-07-08 23:32:46 +0200

    refactor: question_parser
    + simplified logic of txt parser
    + custom answers length
    
    todo: yaml parser

--- roquiz/lib/cli/questions_parser.dart
+++ roquiz/lib/cli/questions_parser.dart
@@ -4,118 +4,122 @@ import 'dart:io';
 import 'package:roquiz/model/quiz/question.dart';
 import 'package:yaml/yaml.dart';
 
-List<Question> parseQuestionsFromTxt(String content, int maxNumAnwers) {
-  const minAnswers = 2;
-
+List<Question> parseQuestionsFromTxt(String content) {
   List<Question> questions = [];
   List<String> topics = [];
-  String currentTopic = "";
+  String? currentTopic;
 
+  // Remove leading and trailing whitespaces before splitting in lines
   content = content.trim();
 
-  // Empty content
   if (content.isEmpty) {
-    throw FormatException("File vuoto");
+    throw FormatException("Empty file");
   }
 
   LineSplitter ls = const LineSplitter();
   List<String> lines = ls.convert(content);
 
-  // Trim leading and trailing spaces
+  // Trim leading and trailing whitespaces
   for (String s in lines) {
     s = s.trim();
   }
 
   // Loop over lines
-  for (int iQ = 0; iQ < lines.length; iQ++) {
-    // Check if the current line is empty or full of white spaces
-    if (lines[iQ].isEmpty) continue;
+  for (int iLine = 0; iLine < lines.length; iLine++) {
+    int id;
+    String body;
+    List<String> answers = [];
+    int? correctAnswer;
+
+    // Skip line if it's empty
+    if (lines[iLine].isEmpty) {
+      continue;
+    }
 
     // Check if the current line is a topic
-    if (lines[iQ].startsWith("@")) {
+    if (lines[iLine].startsWith("@")) {
       // Check if there already are questions without topic
       if (topics.isEmpty && questions.isNotEmpty) {
         throw FormatException(
-            "Riga ${iQ + 1}: errore argomenti! E' stato trovato un argomento ma sono gia' presenti domande senza");
+          "Line ${iLine + 1}: topics error! A topic was found but there already are questions with no topic",
+        );
       }
 
       // Parse topic
-      currentTopic = lines[iQ].substring(1).replaceAll("=", "").trim();
+      currentTopic = lines[iLine].substring(1).replaceAll("=", "").trim();
 
       // Duplicated topic
       if (topics.contains(currentTopic)) {
-        throw FormatException("Riga ${iQ + 1}: argomento duplicato");
+        throw FormatException(
+          "Line ${iLine + 1}: duplicated topic '$currentTopic'",
+        );
       }
       topics.add(currentTopic);
       continue;
     }
 
-    // Parse question
-    Question question = Question.onlyBody(questions.length + 1, lines[iQ]);
-    if (topics.isNotEmpty) {
-      question.setTopic(currentTopic);
-    }
-    iQ++;
+    id = questions.length + 1;
+    body = lines[iLine];
+    iLine++;
 
     // Parse answers
-    for (int iA = 0; iA < maxNumAnwers; iQ++, iA++) {
+    for (int iAnswer = 0; ; iLine++, iAnswer++) {
       // End of file
-      if (iQ >= lines.length) {
-        throw FormatException(
-            "Riga ${iQ + 1}: fine del file prima della lettura di tutte le risposte");
-      }
-
-      // Check if answer line is empty
-      if (lines[iQ].isEmpty) {
-        throw FormatException("Riga ${iQ + 1}: risposta vuota");
-      }
-
-      // Check if we found the correct answer
-      if (lines[iQ].length == 1) {
+      if (iLine >= lines.length) {
         break;
       }
 
-      // Try splitting
-      List<String> splitted = lines[iQ].split(". ");
-      if (splitted.length < 2 || splitted[1].isEmpty) {
+      // Answer iAnswer is empty
+      if (lines[iLine].isEmpty) {
         throw FormatException(
-            "Riga ${iQ + 1}: risposta ${String.fromCharCode((iA + 65))} formattata male");
+          "Line ${iLine + 1}: empty answer ${String.fromCharCode(iAnswer + 65)}",
+        );
       }
 
-      question.addAnswer(splitted[1]);
-    }
+      // Break out if we find the correct answer
+      if (lines[iLine].length == 1) {
+        correctAnswer = lines[iLine].codeUnitAt(0) - 65;
+        break;
+      }
 
-    if (iQ >= lines.length) {
+      // Try parsing answer iAnswer
+      List<String> splitted = lines[iLine].split(". ");
+      if (splitted.length < 2) {
+        throw FormatException(
+          "Line ${iLine + 1}: answer ${String.fromCharCode(iAnswer + 65)} badly formatted",
+        );
+      }
+      if (splitted[1].isEmpty) {
+        throw FormatException(
+          "Line ${iLine + 1}: answer ${String.fromCharCode(iAnswer + 65)} is empty",
+        );
+      }
+      answers.add(splitted[1]);
+    }
+    if (correctAnswer == null) {
       throw FormatException(
-          "Riga ${iQ + 1}: fine del file prima della lettura della risposta corretta");
+        "Line ${iLine + 1}: missing correct answer for question #$id",
+      );
     }
 
-    // Try reading the correct answer
-    if (lines[iQ].length != 1) {
-      throw FormatException("Riga ${iQ + 1}: risposta corretta assente");
+    try {
+      Question question = Question(
+        id,
+        body,
+        currentTopic,
+        answers,
+        correctAnswer,
+      );
+      questions.add(question);
+    } catch (e) {
+      throw FormatException("Line ${iLine + 1}: error in question #$id: $e");
     }
-
-    if (question.getAnswers().length < minAnswers) {
-      throw FormatException(
-          "Riga ${iQ + 1}: non sono ammesse domande con meno di $minAnswers risposte");
-    }
-
-    // Check if the correct answer is valid (in letters range)
-    int letterCode = lines[iQ].codeUnitAt(0);
-    if (letterCode < "A".codeUnitAt(0) ||
-        letterCode > "A".codeUnitAt(0) + question.getAnswers().length - 1) {
-      throw FormatException(
-          "Riga ${iQ + 1}: la risposta corretta dev'essere una delle risposte presenti");
-    }
-    question.setCorrectAnswer(letterCode - 65);
-    iQ++;
-
-    questions.add(question);
   }
 
   return questions;
 }
 
+// TODO: refactor with new Question class
 List<Question> parseQuestionsFromYaml(String content) {
   List<Question> questions = [];
 
@@ -135,7 +139,12 @@ List<Question> parseQuestionsFromYaml(String content) {
         final correctAnswer = el["correct_answer"];
 
         Question q = Question(
-            iQ, body, topic, List<String>.from(answers), correctAnswer);
+          iQ,
+          body,
+          topic,
+          List<String>.from(answers),
+          correctAnswer,
+        );
 
         iQ++;
         questions.add(q);
@@ -154,7 +163,13 @@ Map<String, int> getTopicSizes(List<Question> questions) {
   Map<String, int> topicSizes = {};
 
   for (Question question in questions) {
-    String topic = question.getTopic();
+    String? topic = question.topic;
+
+    // No topics
+    if (topic == null) {
+      return topicSizes;
+    }
+
     if (topicSizes.containsKey(topic)) {
       topicSizes[topic] = topicSizes[topic]! + 1;
     } else {
@@ -164,6 +179,45 @@ Map<String, int> getTopicSizes(List<Question> questions) {
 
   return topicSizes;
 }
+
+// // Parse from YAML
+// void main(List<String> args) async {
+//   if (args.isEmpty) {
+//     print("No arg specified args");
+//     return;
+//   }
+
+//   File file = File(args[0]);
+//   if (!file.existsSync()) {
+//     print("File ${args[0]} doesn't exist");
+//   }
+
+//   file.readAsString().then((content) {
+//     List<Question> questions = parseQuestionsFromYaml(content);
+//     print(questions.length);
+
+//     if (args.length >= 2 && args[1] == "-t") {
+//       Map<String, int> topicSizes = getTopicSizes(questions);
+//       for (String topic in topicSizes.keys) {
+//         print("- $topic: ${topicSizes[topic]}");
+//       }
+//     }
+//     _printTopics(questions);
+//     return;
+
+//     /*List<Question> questions = parseQuestions(content, 5);
+//     for (Question q in questions) {
+//       print(q.toYAML());
+//     }
+
+//     if (args.length >= 2 && args[1] == "-t") {
+//       Map<String, int> topicSizes = getTopicSizes(questions);
+//       for (String topic in topicSizes.keys) {
+//         print("- $topic");
+//       }
+//     }*/
+//   });
+// }
 
 void main(List<String> args) async {
   if (args.isEmpty) {
@@ -177,8 +231,7 @@ void main(List<String> args) async {
   }
 
   file.readAsString().then((content) {
-    List<Question> questions = parseQuestionsFromYaml(content);
-    print(questions.length);
+    List<Question> questions = parseQuestionsFromTxt(content);
 
     if (args.length >= 2 && args[1] == "-t") {
       Map<String, int> topicSizes = getTopicSizes(questions);
