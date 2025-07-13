f6d8fd28f8cb7a1101ff20458741d6c33a0602d9
mikyll <righi.michy@gmail.com>
2025-07-13 23:51:48 +0200

    fix(model): quiz

--- roquiz/lib/model/quiz/quiz.dart
+++ roquiz/lib/model/quiz/quiz.dart
@@ -1,21 +1,14 @@
 import 'package:roquiz/model/quiz/question.dart';
 
 class Quiz {
-  List<Question> _questions = [];
-  List<int> _userAnswers = [];
+  List<Question> questions = [];
+  List<int?> selectedAnswers = [];
 
-  int _iQuestion = 0;
-
-  Quiz();
-
-  Quiz.init(
-      List<Question> selectedQuestions, int questionNum, bool shuffleAnswers) {
+  Quiz(List<Question> selectedQuestions, int questionNum, bool shuffleAnswers) {
     selectedQuestions.shuffle();
     for (int i = 0; i < questionNum; i++) {
-      _questions.add(selectedQuestions[i]);
-      _userAnswers[i] = -1;
+      questions.add(selectedQuestions[i]);
+      selectedAnswers.add(null);
     }
   }
-
-  int get currentQuestion => _iQuestion;
 }
