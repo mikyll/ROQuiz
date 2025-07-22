c1f533fed9cf92c83fbd91a4e0186465af7a2e5e
mikyll <righi.michy@gmail.com>
2025-07-23 00:42:54 +0200

    fix(model): update quiz
    - fix errors in constructor
    - add method to count correct answers

--- roquiz/lib/model/quiz/quiz.dart
+++ roquiz/lib/model/quiz/quiz.dart
@@ -9,10 +9,27 @@ class Quiz {
     required int questionNum,
     required bool shuffleAnswers,
   }) {
+    questions = List.of(questions);
     questions.shuffle();
+    questions = questions.take(questionNum).toList();
+
     for (int i = 0; i < questionNum; i++) {
-      questions.add(questions[i]);
+      if (shuffleAnswers) {
+        questions[i].answers.shuffle();
+      }
+
       selectedAnswers.add(null);
     }
+  }
+
+  int countCorrectAnswers() {
+    int correctAnswers = 0;
+    for (int i = 0; i < questions.length; i++) {
+      if (selectedAnswers[i] != null &&
+          selectedAnswers[i] == questions[i].correctAnswer) {
+        correctAnswers++;
+      }
+    }
+    return correctAnswers;
   }
 }
