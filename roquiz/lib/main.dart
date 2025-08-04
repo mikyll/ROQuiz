4480dfe93edd079bceff7d5a298e4176647a8f3d
mikyll <righi.michy@gmail.com>
2025-08-05 01:00:54 +0200

    fix: main
    replace ThemProvider with Settings

--- roquiz/lib/main.dart
+++ roquiz/lib/main.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
-import 'package:roquiz/model/style/theme_provider.dart';
+import 'package:roquiz/model/persistence/settings.dart';
+import 'package:roquiz/model/persistence/settings_manager.dart';
 import 'package:roquiz/model/style/themes.dart';
 import 'package:roquiz/view/view_menu.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  bool isDarkTheme = false;
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final Settings settings = await SettingsManager.load();
 
   return runApp(
-    ChangeNotifierProvider<ThemeProvider>(
-      child: ROQuizApp(packageInfo: packageInfo),
+    ChangeNotifierProvider<Settings>(
+      child: ROQuizApp(packageInfo: packageInfo, settings: settings),
       create: (BuildContext context) {
-        return ThemeProvider(isDarkTheme);
+        return settings;
       },
     ),
   );
 }
 
 class ROQuizApp extends StatelessWidget {
-  const ROQuizApp({super.key, required this.packageInfo});
-
   final PackageInfo packageInfo;
+  final Settings settings;
+
+  const ROQuizApp({
+    super.key,
+    required this.packageInfo,
+    required this.settings,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
+    return Consumer<Settings>(
       builder: (context, value, child) {
         return MaterialApp(
           title: packageInfo.appName,
-          themeMode: value.themeMode,
+          themeMode: settings.themeDark ? ThemeMode.dark : ThemeMode.light,
           theme: Themes.themeLight,
           darkTheme: Themes.themeDark,
-          home: ViewMenu(packageInfo: packageInfo),
+          home: ViewMenu(packageInfo: packageInfo, settings: settings),
         );
       },
     );
