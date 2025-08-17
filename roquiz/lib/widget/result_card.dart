fb86881ceff6141ca425905ece550a73d7da0299
mikyll <righi.michy@gmail.com>
2025-08-17 18:53:28 +0200

    fix(result_card): show range if written grade not set

--- roquiz/lib/widget/result_card.dart
+++ roquiz/lib/widget/result_card.dart
@@ -1,10 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:roquiz/model/utils/utils.dart';
 
-class ResultCard extends StatelessWidget {
+class ResultCard extends StatefulWidget {
   final int correctAnswers;
   final int questionNum;
   final int? writtenGrade;
@@ -32,6 +31,23 @@ class ResultCard extends StatelessWidget {
        );
 
   @override
+  State<StatefulWidget> createState() => _ResultCardState();
+}
+
+class _ResultCardState extends State<ResultCard> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     String tooltipMessage =
         "Il voto finale è calcolato come segue:\n"
@@ -39,7 +55,7 @@ class ResultCard extends StatelessWidget {
         "Entrambe le prove sono in 30esimi, ma è possibile ottenere punti "
         "extra e arrivare a 32. La lode viene assegnata se il totale supera 30.";
 
-    if (writtenGrade == null) {
+    if (widget.writtenGrade == null) {
       tooltipMessage +=
           "\n\nPer avere una valutazione precisa, inserisci il "
           "voto della prova scritta nelle impostazioni.";
@@ -49,97 +65,112 @@ class ResultCard extends StatelessWidget {
       constraints: BoxConstraints(maxWidth: 320.0),
       child: Stack(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor,
-              borderRadius: BorderRadius.circular(10),
-              border: BoxBorder.all(width: 2, color: Colors.grey),
-              boxShadow: [
-                BoxShadow(
-                  color: Theme.of(context).disabledColor,
-                  spreadRadius: 0.5,
-                  blurRadius: 2,
-                  offset: const Offset(2, 2),
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 70.0,
-                vertical: 25.0,
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return Transform.translate(
+                offset: Offset(0, 100 * _controller.value),
+                child: Stack(
                   children: [
-                    Row(
-                      children: [
-                        Expanded(child: Text("Risposte corrette: ")),
-                        Text("$correctAnswers/$questionNum"),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(child: Text("Voto scritto: ")),
-                        Text(writtenGrade.toString()),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(child: Text("Voto quiz: ")),
-                        Text(
-                          "${quizGrade.toString()} (${min(quizGrade / 30 * 100, 100).round()}%)",
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius: BorderRadius.circular(10),
+                        border: BoxBorder.all(width: 2, color: Colors.grey),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Theme.of(context).disabledColor,
+                            spreadRadius: 0.5,
+                            blurRadius: 2,
+                            offset: const Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 70.0,
+                          vertical: 25.0,
                         ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(child: Text("Tempo: ")),
-                        Text(timerString),
-                      ],
-                    ),
-                    const SizedBox(height: 15.0),
-                    if (totalGrade != null)
-                      Text(
-                        "Voto finale: ${getGradeString(totalGrade!)} (${totalGrade!.toStringAsFixed(1)})",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(child: Text("Risposte corrette: ")),
+                                  Text(
+                                    "${widget.correctAnswers}/${widget.questionNum}",
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(child: Text("Voto scritto: ")),
+                                  Text(widget.writtenGrade.toString()),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(child: Text("Voto quiz: ")),
+                                  Text(
+                                    "${widget.quizGrade.toString()} (${min(widget.quizGrade / 30 * 100, 100).round()}%)",
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(child: Text("Tempo: ")),
+                                  Text(widget.timerString),
+                                ],
+                              ),
+                              const SizedBox(height: 15.0),
+                              if (widget.totalGrade != null)
+                                Text(
+                                  "Voto finale: ${getGradeString(widget.totalGrade!)} (${widget.totalGrade!.toStringAsFixed(1)})",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                              // TODO: style (bold, etc.)
+                              if (widget.totalGradeRange != null)
+                                Text(
+                                  "Range voto finale: [${widget.totalGradeRange!.$1.toStringAsFixed(0)},"
+                                  "${widget.totalGradeRange!.$2.toStringAsFixed(0)}]",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                              // TODO: style (bold, etc.)
+                            ],
+                          ),
                         ),
                       ),
-                    // TODO: style (bold, etc.)
-                    if (totalGradeRange != null)
-                      Text(
-                        "Range voto finale: [${totalGradeRange!.$1.toStringAsFixed(0)},${totalGradeRange!.$2.toStringAsFixed(0)}]",
+                    ),
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: Tooltip(
                         textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
+                        margin: EdgeInsets.all(5.0),
+                        constraints: BoxConstraints(maxWidth: 300.0),
+                        message: tooltipMessage,
+                        child: IconButton.filled(
+                          padding: EdgeInsets.zero,
+                          constraints: BoxConstraints(),
+                          icon: Icon(Icons.help, size: 20),
+                          onPressed: () {},
                         ),
                       ),
-                    // TODO: style (bold, etc.)
+                    ),
                   ],
                 ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 10,
-            right: 10,
-            child: Tooltip(
-              textAlign: TextAlign.center,
-              margin: EdgeInsets.all(5.0),
-              constraints: BoxConstraints(maxWidth: 300.0),
-              message: tooltipMessage,
-              child: IconButton.filled(
-                padding: EdgeInsets.zero,
-                constraints: BoxConstraints(),
-                icon: Icon(Icons.help, size: 20),
-                onPressed: () {},
-              ),
-            ),
+              );
+            },
           ),
         ],
       ),
