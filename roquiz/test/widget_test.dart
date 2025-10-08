0d54f6e7bfe04ef18ceb4ae0ebdfe4b0dbc74c34
mikyll <righi.michy@gmail.com>
2025-10-08 21:59:53 +0200

    test: commented temporary test file

--- roquiz/test/widget_test.dart
+++ roquiz/test/widget_test.dart
@@ -11,22 +11,22 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:roquiz/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    await tester.pumpWidget(ROQuizApp(packageInfo: packageInfo));
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//     // Build our app and trigger a frame.
+//     PackageInfo packageInfo = await PackageInfo.fromPlatform();
+//     await tester.pumpWidget(ROQuizApp(packageInfo: packageInfo, settings: ,));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+//     // Verify that our counter starts at 0.
+//     expect(find.text('0'), findsOneWidget);
+//     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//     // Tap the '+' icon and trigger a frame.
+//     await tester.tap(find.byIcon(Icons.add));
+//     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+//     // Verify that our counter has incremented.
+//     expect(find.text('0'), findsNothing);
+//     expect(find.text('1'), findsOneWidget);
+//   });
+// }
