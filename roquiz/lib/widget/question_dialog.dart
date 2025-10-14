6c1475834ad1824af249fc84d1bdb4c2c0da1b22
mikyll <righi.michy@gmail.com>
2025-10-14 02:51:48 +0200

    fix(widget/question_dialog): prevent vertical overflow

--- roquiz/lib/widget/question_dialog.dart
+++ roquiz/lib/widget/question_dialog.dart
@@ -23,8 +23,9 @@ class _QuestionDialogState extends State<QuestionDialog> {
   final ScrollController _scrollController = ScrollController();
   final TextEditingController _bodyController = TextEditingController();
 
+  String? _body;
   String? _topic;
-  List<String> _answers = ["", ""];
+  List<String> _answers = [""];
   int? _correctAnswer;
 
   Question? getQuestion() {
@@ -40,6 +41,24 @@ class _QuestionDialogState extends State<QuestionDialog> {
     } catch (_) {}
 
     return question;
+  }
+
+  String? validateBody(String? value) {
+    if (value!.isEmpty) {
+      return "Il corpo della domanda dev'essere non vuoto";
+    }
+
+    return "";
+  }
+
+  String? validateAnswer(String? value) {
+    // TODO: validate:
+    // - no empty body
+    // - ok topic
+    // - at least 2 answers
+    // - selected correct answer
+
+    return "";
   }
 
   @override
@@ -61,7 +80,6 @@ class _QuestionDialogState extends State<QuestionDialog> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-
       child: AlertDialog(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
@@ -98,117 +116,137 @@ class _QuestionDialogState extends State<QuestionDialog> {
           ],
         ),
         content: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 400, minWidth: 400),
+          constraints: BoxConstraints(maxHeight: 500, maxWidth: 400),
           child: Padding(
             padding: const EdgeInsets.all(0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              spacing: 10.0,
-              children: [
-                TextFormField(
-                  key: GlobalKey(),
-                  controller: _bodyController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    label: Text("Corpo"),
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                  ),
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                  minLines: 4,
-                ),
-                if (_topic != null)
-                  DropdownButtonFormField(
-                    value: _topic,
-                    items: widget.topicsList!.map<DropdownMenuItem<String>>((
-                      String value,
-                    ) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 10.0,
+                children: [
+                  SizedBox(height: 10),
+                  TextFormField(
+                    key: GlobalKey(),
+                    validator: validateBody,
+                    controller: _bodyController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      label: Text("Argomento"),
+                      label: Text("Corpo"),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
                     ),
-                    onChanged: (_) {},
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    minLines: 4,
                   ),
+                  if (_topic != null)
+                    DropdownButtonFormField(
+                      value: _topic,
+                      items: widget.topicsList!.map<DropdownMenuItem<String>>((
+                        String value,
+                      ) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text("Argomento"),
+                      ),
+                      onChanged: (_) {},
+                    ),
 
-                SingleChildScrollView(
-                  controller: _scrollController,
-                  physics: NeverScrollableScrollPhysics(),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    spacing: 10.0,
-                    children: List.generate(_answers.length, (index) {
-                      return Row(
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              setState(() {
-                                if (_correctAnswer == index) {
-                                  _correctAnswer = null;
-                                  return;
-                                }
+                  SingleChildScrollView(
+                    controller: _scrollController,
+                    physics: NeverScrollableScrollPhysics(),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 10.0,
+                      children: List.generate(_answers.length, (index) {
+                        bool isAnswer = _answers[index].isNotEmpty;
 
-                                _correctAnswer = index;
-                              });
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Icon(
-                                _correctAnswer != null &&
-                                        _correctAnswer! == index
-                                    ? Icons.radio_button_on
-                                    : Icons.radio_button_off,
+                        return Row(
+                          children: [
+                            Opacity(
+                              opacity: isAnswer ? 1.0 : 0.0,
+                              child: InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    if (_correctAnswer == index) {
+                                      _correctAnswer = null;
+                                      return;
+                                    }
+
+                                    _correctAnswer = index;
+                                  });
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Icon(
+                                    _correctAnswer != null &&
+                                            _correctAnswer! == index
+                                        ? Icons.radio_button_on
+                                        : Icons.radio_button_off,
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                          Flexible(
-                            child: TextFormField(
-                              key: GlobalKey(),
-                              initialValue: _answers[index],
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                label: Text("Risposta ${index + 1}"),
-                                floatingLabelBehavior:
-                                    FloatingLabelBehavior.always,
+                            Flexible(
+                              child: TextFormField(
+                                key: GlobalKey(),
+                                initialValue: _answers[index],
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  label: Text("Risposta ${index + 1}"),
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.always,
+                                ),
+
+                                onChanged: (value) {
+                                  setState(() {
+                                    _answers[index] = value;
+                                  });
+                                },
+                                // TODO: on change, if not empty, add new empty answer
                               ),
                             ),
-                          ),
-                          SizedBox(width: 10),
-                          InkWell(
-                            child: Icon(Icons.close),
-                            onTap: () {
-                              if (_answers.length > 2) {
-                                setState(() {
-                                  _answers.removeAt(index);
+                            SizedBox(width: 10),
 
-                                  if (_correctAnswer == index) {
-                                    _correctAnswer = null;
+                            Opacity(
+                              opacity: isAnswer ? 1.0 : 0.0,
+                              child: InkWell(
+                                child: Icon(Icons.close),
+                                onTap: () {
+                                  if (_answers.length > 2) {
+                                    setState(() {
+                                      _answers.removeAt(index);
+
+                                      if (_correctAnswer == index) {
+                                        _correctAnswer = null;
+                                      }
+                                    });
                                   }
-                                });
-                              }
-                            },
-                          ),
-                        ],
-                      );
-                    }),
+                                },
+                              ),
+                            ),
+                          ],
+                        );
+                      }),
+                    ),
                   ),
-                ),
-                ElevatedButton.icon(
-                  label: Icon(Icons.add),
-                  onPressed: () {
-                    if (_answers.length < 6) {
-                      setState(() {
-                        _answers.add("");
-                      });
-                    }
-                  },
-                ),
-              ],
+                  ElevatedButton.icon(
+                    label: Icon(Icons.add),
+                    onPressed: () {
+                      if (_answers.length < 6) {
+                        setState(() {
+                          _answers.add("");
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -216,7 +254,12 @@ class _QuestionDialogState extends State<QuestionDialog> {
         actions: [
           ElevatedButton(
             onPressed: () {
-              // TODO: validate
+              // TODO: call validate
+
+              // TODO
+              //widget.onSubmit();
+
+              //if (validateBody(value))
 
               Navigator.pop(context);
             },
