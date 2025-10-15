63f3bd30aa7804f3f987a6ea1c23d2f5dc0590a6
mikyll <righi.michy@gmail.com>
2025-10-16 00:07:01 +0200

    feat(cli/question_parser): add utilities for duplicate check

--- roquiz/lib/cli/questions_parser.dart
+++ roquiz/lib/cli/questions_parser.dart
@@ -214,6 +214,61 @@ Map<String, int> getTopicSizes(List<Question> questions) {
   return topicSizes;
 }
 
+bool isAnswerDuplicate(
+  List<String> answers,
+  String answer, {
+  bool ignoreCase = true,
+}) {
+  for (String s in answers) {
+    if (ignoreCase) {
+      s = s.toLowerCase();
+      answer = answer.toLowerCase();
+    }
+    if (s == answer) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool isQuestionBodyDuplicate(
+  List<Question> questions,
+  String questionBody, {
+  bool ignoreCase = true,
+}) {
+  for (Question q in questions) {
+    if (ignoreCase) {
+      q.body = q.body.toLowerCase();
+      questionBody = questionBody.toLowerCase();
+    }
+    if (q.body == questionBody) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool isQuestionDuplicate(
+  List<Question> questions,
+  Question question, {
+  bool ignoreCase = true,
+}) {
+  for (Question q in questions) {
+    if (isQuestionBodyDuplicate(questions, q.body, ignoreCase: ignoreCase)) {
+      return true;
+    }
+    for (String a in q.answers) {
+      if (isAnswerDuplicate(q.answers, a, ignoreCase: ignoreCase)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 void _printTopics(List<Question> questions) {
   String res = "Topics:\n";
   int curr = 0;
