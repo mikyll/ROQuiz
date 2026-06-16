5b6a0cf5d3a6594dffad4f75435b975655e98d10
mikyll <righi.michy@gmail.com>
2026-06-17 00:38:19 +0200

    feat(history): rework history view with per-quiz detail screen

--- roquiz/lib/view/view_history.dart
+++ roquiz/lib/view/view_history.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:roquiz/model/quiz/question.dart';
 import 'package:roquiz/model/quiz/quiz_completed.dart';
 import 'package:roquiz/model/utils/grade.dart';
 import 'package:roquiz/model/utils/time.dart';
-import 'package:roquiz/view/view_questions_edit.dart';
+import 'package:roquiz/view/view_history_quiz.dart';
 import 'package:roquiz/widget/constrained_appbar.dart';
 import 'package:roquiz/widget/custom_back_button.dart';
-import 'package:roquiz/widget/question_card.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class ViewHistory extends StatefulWidget {
   const ViewHistory({super.key, required this.quizList});
@@ -20,21 +17,30 @@ class ViewHistory extends StatefulWidget {
 
 class ViewHistoryState extends State<ViewHistory> {
   final ScrollController _scrollController = ScrollController();
-
-  late List<QuizCompleted> _orderedQuizList;
+  late List<QuizCompleted> _quizList;
 
   @override
   void initState() {
     super.initState();
+    _quizList = List.of(widget.quizList);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _removeQuiz(int index) {
+    setState(() {
+      _quizList.removeAt(index);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: true,
-      // onPopInvoked: (_) {
-      //   // todo
-      // },
       child: Scaffold(
         appBar: ConstrainedAppBar(
           maxWidth: 500.0,
@@ -47,88 +53,42 @@ class ViewHistoryState extends State<ViewHistory> {
               constraints: BoxConstraints(maxWidth: 500.0),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: ListView.builder(
-                  controller: _scrollController,
-                  itemCount: widget.quizList.length,
-                  itemBuilder: (context, index) {
-                    QuizCompleted quizCompleted = widget.quizList[index];
-
-                    // TODO
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 5.0),
-                      child: _QuizCompletedWidget(
-                        timestamp: quizCompleted.timestamp,
-                        correctQuestions: quizCompleted.correctAnswers,
-                        totalQuestions: quizCompleted.questions.length,
-                        timeSpent: quizCompleted.timeSpent,
-                        grade: quizCompleted.grade,
-                        onTap: () {
-                          // TODO
-                        },
-                        onRemove: () {
-                          // TODO
+                child: _quizList.isEmpty
+                    ? Center(child: Text("Nessun quiz completato"))
+                    : ListView.builder(
+                        controller: _scrollController,
+                        itemCount: _quizList.length,
+                        itemBuilder: (context, index) {
+                          final QuizCompleted q = _quizList[index];
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 5.0),
+                            child: _QuizCompletedWidget(
+                              timestamp: q.timestamp,
+                              correctQuestions: q.correctAnswers,
+                              totalQuestions: q.questions.length,
+                              timeSpent: q.timeSpent,
+                              grade: q.grade,
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        ViewHistoryQuiz(quizCompleted: q),
+                                  ),
+                                );
+                              },
+                              onRemove: () => _removeQuiz(index),
+                            ),
+                          );
                         },
                       ),
-                    );
-                  },
-                ),
               ),
             ),
           ),
-
-          // ListView.builder(
-          //   controller: _scrollController,
-          //   itemCount: _questions.length,
-          //   itemBuilder: (_, index) {
-          //     Widget questionWidget = QuestionCard.base(
-          //       question: _questions[index],
-          //       hideCorrectAnswer: !_showAnswers,
-          //     );
-          //     // Check if we have to display the topic divider
-          //     if (index == 0 ||
-          //         _questions[index - 1].topic != _questions[index].topic) {
-          //       return Column(
-          //         children: [
-          //           Padding(
-          //             padding: EdgeInsets.only(
-          //               left: 10.0,
-          //               right: 10.0,
-          //               top: index == 0 ? 10.0 : 0,
-          //             ),
-          //             child: Row(
-          //               children: [
-          //                 Padding(
-          //                   padding: const EdgeInsets.only(right: 10.0),
-          //                   child: Text(
-          //                     _questions[index].topic!,
-          //                     style: const TextStyle(
-          //                       fontSize: 18.0,
-          //                       color: Colors.grey,
-          //                     ),
-          //                   ),
-          //                 ),
-          //                 const Expanded(child: Divider(thickness: 2)),
-          //               ],
-          //             ),
-          //           ),
-          //           questionWidget,
-          //         ],
-          //       );
-          //     }
-          //     // Otherwise simply return the card
-          //     else {
-          //       return questionWidget;
-          //     }
-          //   },
-          // ),
         ),
-
         bottomNavigationBar: Container(
           decoration: BoxDecoration(
             color: Theme.of(context).highlightColor.withAlpha(70),
-            // border: Border(
-            //   top: BorderSide(color: Theme.of(context).disabledColor),
-            // ),
           ),
           child: Row(
             children: [
@@ -176,63 +136,31 @@ class _QuizCompletedWidget extends StatelessWidget {
   final int correctQuestions;
   final int totalQuestions;
   final int timeSpent;
-  final double? grade;
+  final double grade;
   final void Function()? onTap;
   final void Function()? onRemove;
 
   @override
   Widget build(BuildContext context) {
-    // TODO
-
-    // InkWell(
-    //   onTap: onTap,
-    //   child: Row(
-    //     children: [
-    //       Text(timestamp.toString()),
-    //       Text("$correctQuestions/$totalQuestions"),
-    //       Text(getTimeString(timeSpent)),
-    //       if (grade != null) Text(getGradeString(grade!)),
-    //     ],
-    //   ),
-    // ),
-    // Positioned(
-    //   right: 10,
-    //   child: InkWell(onTap: onRemove, child: Icon(Icons.delete)),
-    // ),
-
-    return Stack(
-      children: [
-        ListTile(
-          leading: grade != null
-              ? Text(
-                  getGradeString(grade!),
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                )
-              : null,
-          title: Row(
-            spacing: 10.0,
-            children: [
-              Text(
-                timestamp
-                    .toString()
-                    .replaceAll("T", " ")
-                    .split(".")[0]
-                    .padRight(22),
-              ),
-              Text(
-                "${correctQuestions.toString().padLeft(3)}/${totalQuestions.toString().padRight(3)}",
-              ),
-              Text(getTimeString(timeSpent)),
-            ],
-          ),
-          isThreeLine: false,
-          onTap: onTap,
-          trailing: InkWell(
-            onTap: onRemove,
-            child: Icon(Icons.delete, size: 30),
-          ),
-        ),
-      ],
+    return ListTile(
+      leading: Text(
+        getGradeString(grade),
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      title: Row(
+        spacing: 10.0,
+        children: [
+          Text(timestamp.toString().replaceAll("T", " ").split(".")[0]),
+          Text("$correctQuestions/$totalQuestions"),
+          Text(getTimeString(timeSpent)),
+        ],
+      ),
+      isThreeLine: false,
+      onTap: onTap,
+      trailing: InkWell(
+        onTap: onRemove,
+        child: Icon(Icons.delete, size: 30),
+      ),
     );
   }
 }
---
+++ roquiz/lib/view/view_history_quiz.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:roquiz/model/quiz/quiz_completed.dart';
+import 'package:roquiz/model/utils/grade.dart';
+import 'package:roquiz/model/utils/time.dart';
+import 'package:roquiz/widget/constrained_appbar.dart';
+import 'package:roquiz/widget/custom_back_button.dart';
+import 'package:roquiz/widget/question_card.dart';
+
+class ViewHistoryQuiz extends StatefulWidget {
+  const ViewHistoryQuiz({super.key, required this.quizCompleted});
+
+  final QuizCompleted quizCompleted;
+
+  @override
+  State<StatefulWidget> createState() => _ViewHistoryQuizState();
+}
+
+class _ViewHistoryQuizState extends State<ViewHistoryQuiz> {
+  final ScrollController _scrollController = ScrollController();
+
+  int _iQuestion = 0;
+  int _dragDirectionDX = 0;
+
+  void _previousQuestion() {
+    setState(() {
+      if (_iQuestion > 0) {
+        _iQuestion--;
+        _scrollController.jumpTo(0);
+      }
+    });
+  }
+
+  void _nextQuestion() {
+    setState(() {
+      if (_iQuestion < widget.quizCompleted.questions.length - 1) {
+        _iQuestion++;
+        _scrollController.jumpTo(0);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final QuizCompleted quiz = widget.quizCompleted;
+    final int totalQuestions = quiz.questions.length;
+
+    return PopScope(
+      canPop: true,
+      child: GestureDetector(
+        onPanUpdate: (details) {
+          setState(() {
+            details.delta.dx > 5
+                ? _dragDirectionDX = -1
+                : (details.delta.dx < -5 ? _dragDirectionDX = 1 : null);
+          });
+        },
+        onPanEnd: (details) {
+          _dragDirectionDX > 0
+              ? _nextQuestion()
+              : (_dragDirectionDX < 0 ? _previousQuestion() : null);
+          _dragDirectionDX = 0;
+        },
+        child: Scaffold(
+          appBar: ConstrainedAppBar(
+            maxWidth: 500.0,
+            title: Text("Dettaglio Quiz"),
+            leading: CustomBackButton(),
+          ),
+          body: SelectionArea(
+            child: SafeArea(
+              child: Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 500.0),
+                  child: totalQuestions == 0
+                      ? Center(child: Text("Nessuna domanda disponibile"))
+                      : Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                top: 10.0,
+                                left: 20.0,
+                                right: 20.0,
+                              ),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "D${_iQuestion + 1}/$totalQuestions",
+                                    style: TextTheme.of(context).headlineSmall!
+                                        .copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    "${quiz.correctAnswers}/$totalQuestions  ${getTimeString(quiz.timeSpent)}",
+                                    style: TextTheme.of(context).headlineSmall!
+                                        .copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: SingleChildScrollView(
+                                controller: _scrollController,
+                                child: QuestionCard.quizOver(
+                                  question: quiz.questions[_iQuestion],
+                                  selectedAnswer:
+                                      quiz.selectedAnswers[_iQuestion],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                ),
+              ),
+            ),
+          ),
+          bottomNavigationBar: totalQuestions == 0
+              ? null
+              : Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).highlightColor.withAlpha(70),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxWidth: 500.0,
+                          maxHeight: 70.0,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              Positioned(
+                                left: 0,
+                                bottom: 0,
+                                child: Row(
+                                  spacing: 20.0,
+                                  children: [
+                                    IconButton(
+                                      onPressed: _previousQuestion,
+                                      icon: Icon(Icons.arrow_back_ios_rounded),
+                                      iconSize: 35,
+                                    ),
+                                    IconButton(
+                                      onPressed: _nextQuestion,
+                                      icon: Icon(
+                                        Icons.arrow_forward_ios_rounded,
+                                      ),
+                                      iconSize: 35,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Positioned(
+                                right: 0,
+                                bottom: 0,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    getGradeString(quiz.grade),
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+}
